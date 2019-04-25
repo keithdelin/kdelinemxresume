c9bfc9f6040a19cee3d10743dd8946cca47841b1
Keith De-Lin <keith.de-lin@sizmek.com>
2019-04-25 14:14:26 -0400

    changes

--- findParams.js
+++ findParams.js
@@ -1,6 +1,9 @@
 exports.findParams = (req, res) => {
     console.log(`params = ${ req.query.id }`);
     //res.send(`${req.query.id}, ${req.query.name}`);
-    res.send(`OK`);
-    res.send(`Keith`);
+    res.send(`ABCD
+    A=><>
+    B<=<>
+    C>>=>
+    D<<<=`);
 };
--- server.js
+++ server.js
@@ -4,34 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-const winston = require('winston');
-
-const logger = winston.createLogger({
-	level: 'info',
-	format: winston.format.json(),
-	defaultMeta: { service: 'user-service' },
-	transports: [
-	  //
-	  // - Write to all logs with level `info` and below to `combined.log` 
-	  // - Write all logs error (and below) to `error.log`.
-	  //
-	  new winston.transports.File({ filename: 'error.log', level: 'error' }),
-	  new winston.transports.File({ filename: 'combined.log' })
-	]
-  });
-  
-  //
-  // If we're not in production then log to the `console` with the format:
-  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  // 
-  if (process.env.NODE_ENV !== 'production') {
-	logger.add(new winston.transports.Console({
-	  format: winston.format.simple()
-	}));
-  }
-  
-
-
 app.get('/', findParams.findParams);
 
 app.listen(port, () => {
