2ec4a06eada4040b2c66f8760478ec2f49dc6d0d
Keith De-Lin <keith.de-lin@sizmek.com>
2019-04-25 13:20:44 -0400

    update changes

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-// app.set('view engine', 'ejs');
-
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', (req, res) => {
-// 	res.render('index');
-// });
-
 app.get('/', findParams.findParams);
 
 app.listen(port, () => {
