ee7c77b7bd8bb5e442a75e7b69724961e17ac59a
Keith De-Lin <keith.de-lin@sizmek.com>
2019-04-25 15:38:58 -0400

    no message

--- findParams.js
+++ findParams.js
@@ -1,20 +1,20 @@
 exports.findParams = (req, res) => {
     console.log(`params = ${ req.query.id }`);
     //res.send(`${req.query.id}, ${req.query.name}`);
-    var i = 1,
-        max = 10;
+    // var i = 1,
+    //     max = 10;
     
-    //send multiple responses to the client
-    for (; i <= max; i++) {
-        if (i == 1){
-            res.write('OK');
-        } else {
-            res.write('Keith');
-        }        
-    }
+    // //send multiple responses to the client
+    // for (; i <= max; i++) {
+    //     if (i == 1){
+    //         res.write('OK');
+    //     } else {
+    //         res.write('Keith');
+    //     }        
+    // }
 
-    res.end();
+    // res.end();
     
     
-    //res.send(`OK, Keith`);
+    res.send(`OK`);
 };
