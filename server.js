5c0642a3df69807ee270745115516dc10679273c
Keith De-Lin <keith.de-lin@sizmek.com>
2019-04-25 15:22:42 -0400

    no message

--- server.js
+++ server.js
@@ -7,7 +7,5 @@ const port = process.env.PORT || 8080;
 app.get('/', findParams.findParams);
 
 app.listen(port, () => {
-	debug('Our app is running on http://localhost:' + port);
-	
 	console.warn('Our app is running on http://localhost:' + port);
 });
