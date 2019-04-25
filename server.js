d8f53a73931652c2409eaa61ff42046a909e35ab
Keith De-Lin <keith.de-lin@sizmek.com>
2019-04-25 16:10:43 -0400

    no message

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/?q=Phone&d=Please+provide+a+phone+number+we+can+use+to+reach+you.', findParams.findPhone);
+app.get('/', findParams.findOK);
 //app.get('/first', findParams.findFirstName);
 
 app.listen(port, () => {
