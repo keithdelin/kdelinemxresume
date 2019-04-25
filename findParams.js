bd080c571ac36137bf616a142d13adb1e0debd8c
Keith De-Lin <keith.de-lin@sizmek.com>
2019-04-25 13:48:12 -0400

    update changes

--- findParams.js
+++ findParams.js
@@ -1,5 +1,6 @@
 exports.findParams = (req, res) => {
     console.log(`params = ${ req.query.id }`);
     //res.send(`${req.query.id}, ${req.query.name}`);
-    res.send(`OK`);
+    res.send(`OK
+    Keith`);
 };
--- server.js
+++ server.js
@@ -1,39 +1,22 @@
 const express = require('express');
 const findParams = require('./findParams');
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
-var EventEmitter2 = require('eventemitter2').EventEmitter2;
-var server = new EventEmitter2({
 
-  //
-  // set this to `true` to use wildcards. It defaults to `false`.
-  //
-  wildcard: true,
 
-  //
-  // the delimiter used to segment namespaces, defaults to `.`.
-  //
-  delimiter: '::', 
-  
-  //
-  // set this to `true` if you want to emit the newListener event. The default value is `true`.
-  //
-  newListener: false, 
+// const EventEmitter = require('events');
+// const eventEmitter = new EventEmitter();
+// const EventEmitter2 = require('eventemitter2').EventEmitter2;
 
-  //
-  // the maximum amount of listeners that can be assigned to an event, default 10.
-  //
-  maxListeners: 20,
-  
-  //
-  // show event name in memory leak message when more than maximum amount of listeners is assigned, default false
-  //
-  verboseMemoryLeak: false
-});
+// const server = new EventEmitter2({
+//   wildcard: true,
+//   delimiter: '::',
+//   newListener: false,
+//   maxListeners: 20,
+//   verboseMemoryLeak: false
+// });
 
-server.on('foo.*', function(value1, value2) {
-	console.log(this.event, value1, value2);
-  });
+// server.on('foo.*', function(value1, value2) {
+// 	console.log(this.event, value1, value2);
+//   });
   
 const app = express();
 
