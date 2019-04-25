61c081762083d67dcc293d96069e7ef5f0105c54
Keith De-Lin <keith.de-lin@sizmek.com>
2019-04-25 12:46:45 -0400

    update changes

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
-app.get('/', function(req, res) {
-
-	// ejs render automatically looks in the views folder
+app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
 	console.log('Our app is running on http://localhost:' + port);
 });
